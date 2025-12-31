62082fe3601754f30d83602325c527bc7589c00e
chris_ic3 <chris_ic3@hotmail.com>
2025-12-30 21:15:49 -0600

    Add Details Route and View and Links for Navigation

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/products', async (req,res) => {
     const products = await Product.find({});
     res.render('products/index', { products });
-})
+});
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/details', { product });
+});
 
 app.listen(3000, () => {
     console.log('APP LISTENING ON PORT 3000');
