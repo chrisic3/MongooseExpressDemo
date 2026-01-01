ab653ff1dd2dd4b58012656a57f4c4a56b55068e
chris_ic3 <chris_ic3@hotmail.com>
2025-12-31 23:25:13 -0600

    Add Routes and View to Add a New Product

--- index.js
+++ index.js
@@ -15,12 +15,27 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}));
 
+// RESTful routes
+// READ all products
 app.get('/products', async (req,res) => {
     const products = await Product.find({});
     res.render('products/index', { products });
 });
 
+// CREATE a new product
+app.get('/products/new', (req, res) => {
+    res.render('products/new');
+});
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${ newProduct._id }`);
+});
+
+// READ single product
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
