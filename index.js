abbe9b43f3ee10584f01f5e667089da4a17af0dd
chris_ic3 <chris_ic3@hotmail.com>
2026-01-04 13:15:21 -0600

    Add Feature to Filter by Category from the Details Page

--- index.js
+++ index.js
@@ -24,8 +24,14 @@ app.use(methodOverride('_method'));
 // RESTful routes
 // READ all products
 app.get('/products', async (req,res) => {
-    const products = await Product.find({});
-    res.render('products/index', { products });
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category: category });
+        res.render('products/index', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category: 'All' });
+    }
 });
 
 // CREATE a new product
